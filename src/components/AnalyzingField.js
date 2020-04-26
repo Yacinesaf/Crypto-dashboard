abb729c3e7814cc517e16c14ba1ec89fce2aa0bb
Yacinesaf <yacinesaf96@gmail.com>
2020-04-26 01:52:13 -0400

    analyzing is done but still need some work

--- src/components/AnalyzingField.js
+++ src/components/AnalyzingField.js
@@ -1,17 +1,43 @@
 import React from 'react';
-import { Paper, Table, TableBody } from '@material-ui/core';
+import { Paper, Typography } from '@material-ui/core';
 import emptystate from '../assets/emptystate.svg'
+import { makeStyles } from '@material-ui/core/styles';
 
 function AnalyzingField(props) {
+  const useStyles = makeStyles({
+    typo: {
+      color: 'white',
+      paddingRight: 20
+    },
+    div: {
+      display: 'flex',
+    }
+
+  })
+
+  const classes = useStyles();
 
   return (
     <div>
-      <Paper style={{ backgroundColor: '#24204b', borderRadius: 20 }}>
-        <Table>
-          <TableBody>
-                        {/*<img src={emptystate} alt='empty' height='200px' width='200px' />*/}
-          </TableBody>
-        </Table>
+      <Paper className={classes.div} style={{ backgroundColor: '#24204b', borderRadius: 20, padding: 60, alignItems : 'center' }}>
+        {/*<img src={emptystate} alt='empty' height='200px' width='200px' />*/}
+        <div style={{flexGrow : 1}}>
+          <div className={classes.div} >
+            <Typography className={classes.typo} style={{ color: 'white' }}>1440$</Typography>
+            <Typography style={{ color: 'white' }}>Gross</Typography>
+          </div>
+          <div className={classes.div} >
+            <Typography className={classes.typo} style={{ color: 'white' }}>14220$</Typography>
+            <Typography style={{ color: 'white' }}>Buying fees</Typography>
+          </div>
+          <div className={classes.div} >
+            <Typography className={classes.typo} style={{ color: 'white' }}>14430$</Typography>
+            <Typography style={{ color: 'white' }}>Selling fees</Typography>
+          </div>
+        </div>
+        <div style={{ height: 150, width: 150, border: '5px solid red', borderRadius: 150, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <Typography variant='h4' style={{ color: 'red', }}>% 24</Typography>
+        </div>
       </Paper>
     </div>
   );
--- src/components/Desktop.js
+++ src/components/Desktop.js
@@ -6,13 +6,17 @@ import AnalyzingField from './AnalyzingField';
 function Desktop(props) {
   return (
     <div>
-      <Grid container>
-        <Grid item xs={4} style={{ padding: '40px 80px' }}>
+      <Grid container style={{ padding: '40px 80px' }}>
+        <Grid item xs={4}  >
           <Typography variant='h4' style={{ color: 'white', paddingBottom: 20 }}>Dashboard</Typography>
           <MyCryptoes />
         </Grid>
-        <Grid item xs={8}>
-          <AnalyzingField />
+        <Grid item xs={8} style={{paddingTop : 61}}>
+          <Grid container justify='flex-end'>
+            <Grid item xs={11}>
+              <AnalyzingField />
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
     </div>

4820d005fe01f3303e00c2fd54ce68807ccae402
Yacinesaf <yacinesaf96@gmail.com>
2020-05-17 01:43:16 -0400

    added a hard coded chart for now

--- src/components/ChartVariations.js
+++ src/components/ChartVariations.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Line } from 'react-chartjs-2'
-
+import { InputAdornment, Input, Menu, MenuItem, Typography } from '@material-ui/core';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import { nameFormat } from '../services/helperFunctions'
+import store from '../reduxStore/store'
 
 
 function ChartVariations(props) {
+  const generateCurrencyNumbers = () => {
+    return [0, 1, 2, 3, 4, 5, 6].map(x => {
+      return parseFloat((Math.random() * 40000).toFixed(2))
+    })
+  }
+  const [currencyFlow, setCurrencyFlow] = useState(generateCurrencyNumbers())
+  const [label, setLabel] = useState('Bitcoin')
+  const [anchorEl, setAnchorEl] = useState(null)
+  const [symbol, setSymbol] = useState(null)
+
   const lineGraph = () => {
     const data = {
-      labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+      labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
       datasets: [
         {
-          label: 'Bitcoin',
+          label: label,
           fill: false,
           lineTension: 0.1,
           backgroundColor: 'rgba(75,192,192,0.4)',
@@ -27,14 +40,53 @@ function ChartVariations(props) {
           pointHoverBorderWidth: 2,
           pointRadius: 1,
           pointHitRadius: 10,
-          data: [65, 77, 59, 80, 81, 56, 55, 44, 6, 8, 95, 40]
+          data: currencyFlow
         }
       ]
     };
     return <Line data={data} width={800} height={300} options={{ maintainAspectRatio: false }} />
   }
+
+
+
+
   return (
     <div>
+      <form onClick={(e) => setAnchorEl(e.currentTarget)} style={{ width: 'fit-content' }}>
+        <Input endAdornment={<InputAdornment position="end"><ArrowDropDownIcon /></InputAdornment>} value={symbol ? symbol : 'Crypto'} inputProps={{ 'aria-label': 'description', readOnly: true }} />
+      </form>
+      <Menu
+        style={{ zIndex: 2000 }}
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={(e) => { setAnchorEl(null) }}
+        getContentAnchorEl={null}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+        elevation={4}
+      >
+        {Object.keys(store.getState().cryptoesPrice.cryptoes).map((x, i) => (
+          <MenuItem key={i}
+            style={{ width: 214 }}
+            onClick={(e) => {
+              setSymbol(e.currentTarget.innerText);
+              setAnchorEl(null);
+              setLabel(nameFormat(e.currentTarget.innerText));
+              setCurrencyFlow(generateCurrencyNumbers());
+            }}
+          >
+            <Typography>{x}</Typography>
+          </MenuItem>
+        ))
+        }
+      </Menu>
       {lineGraph()}
     </div>
   );
